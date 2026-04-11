1f9ba89c9099509448fafc61e3f5541fd7b1cb33
eldarrr21 <eldarkryeziu2.0@gmail.com>
2026-04-11 19:28:37 +0200

    solved task1 and added task pdf

---
+++ assignment1/assignment1_task1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int A[] = {15, 5, 1, 4, 2, 8, 7};
+    int n = 7;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n - 1; j > i; j--)
+        {
+            if (A[j - 1] > A[j])
+            {
+                int tmp = A[j];
+                A[j] = A[j - 1];
+                A[j - 1] = tmp;
+            }
+        }
+    }
+    for (int k = 0; k < n; k++)
+    {
+        printf("%d ", A[k]);
+    }
+    printf("\n");
+
+    return 0;
+}
